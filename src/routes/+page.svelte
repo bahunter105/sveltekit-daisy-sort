476d5394d08195b60e9988c9f57631a1659dabc1
Hunter Bourgeois <bahunter@gmail.com>
2023-02-13 23:39:46 -0600

    drawer and canvas added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,26 @@
 <div class="hero min-h-screen" style="background-image: url({hero});" alt="Sorting Photo by Kelly Sikkema on Unsplash">
   <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
-      <div class="max-w-md">
+     <div class="flex flex-col w-full border-opacity-50">
+      <div class="place-items-center">
         <h1 class="mb-5 text-5xl font-bold">Hi there!</h1>
         <p class="mb-5">Want to see the NRMP Matching Algorithm in action?</p>
         <a href="/sorting" class="btn btn-primary">Check it now</a>
       </div>
+      <div class="divider">OR</div>
+      <div class="rounded-box place-items-center">
+        <p class="mb-5">Would you like to see the WIP Zenhance canvas?</p>
+        <a href="/canvas" class="btn btn-secondary">Take a Peak</a>
+      </div>
+    </div>
+
+      <!-- <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Hi there!</h1>
+        <p class="mb-5">Want to see the NRMP Matching Algorithm in action?</p>
+        <a href="/sorting" class="btn btn-primary">Check it now</a>
+        <div class="divider-vertacle"> OR </div>
+        <p class="mb-5">Would you like to see WIP Zenhance canvas?</p>
+        <a href="/canvas" class="btn btn-secondary">Take a Peak</a>
+      </div> -->
     </div>
 </div>
