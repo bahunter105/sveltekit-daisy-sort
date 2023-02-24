aed859d1cd9281d9a524c53d1ceece0a332b3f61
Hunter Bourgeois <bahunter@gmail.com>
2023-02-24 13:23:17 -0600

    added JSON to bring in team and people data & ran build

--- .netlify/server/nodes/3.js
+++ .netlify/server/nodes/3.js
@@ -2,7 +2,7 @@
 
 export const index = 3;
 export const component = async () => (await import('../entries/pages/canvas/_page.svelte.js')).default;
-export const file = '_app/immutable/components/pages/canvas/_page.svelte-ceaa93b2.js';
-export const imports = ["_app/immutable/components/pages/canvas/_page.svelte-ceaa93b2.js","_app/immutable/chunks/index-08fba574.js"];
+export const file = '_app/immutable/components/pages/canvas/_page.svelte-aef8a821.js';
+export const imports = ["_app/immutable/components/pages/canvas/_page.svelte-aef8a821.js","_app/immutable/chunks/index-08fba574.js"];
 export const stylesheets = [];
 export const fonts = [];
