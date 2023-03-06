92209c2680426140b79dc1c0b481ba3fff8424b3
Hunter Bourgeois <bahunter@gmail.com>
2023-03-05 22:16:35 -0600

    udpated team add button and position of items on canvas

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -14,7 +14,7 @@
         <h3 class="text-lg font-bold">Create Your Team!</h3>
         <form class="flex flex-col items-center" method="post" action="/canvas">
           <input class="my-1 input input-sm input-bordered w-full" name="name" type="text" placeholder="Team Name"/>
-          <select class="my-1 input input-sm input-bordered w-full" name="number">
+          <!-- <select class="my-1 input input-sm input-bordered w-full" name="number">
             <option disabled selected>Number of People</option>
             <option>1</option>
             <option>2</option>
@@ -29,7 +29,7 @@
             <option disabled selected>Group</option>
             <option>Group A</option>
             <option>Group B</option>
-          </select>
+          </select> -->
           <input class="my-1 input input-sm input-bordered w-full" name="goal" type="text" placeholder="Goal (You can edit this later)"/>
           <button class="my-2 btn btn-accent" >Submit</button>
         </form>
